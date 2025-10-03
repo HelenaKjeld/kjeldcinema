e5e110eebf18c6947c286f0a7df024b4ffb33dbc
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2025-10-03 15:20:30 +0200

    sql startede

--- Table.sql
+++ Table.sql
@@ -4,3 +4,20 @@ USE Rowancinema;
 SET default_storage_engine=InnoDB;
 
 
+CREATE TABLE User (
+    UserID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Firstname VARCHAR(255),
+    Lastname VARCHAR(255),
+    Email VARCHAR(255) NOT NULL UNIQUE,
+    Password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (TicketsID),
+    FOREIGN KEY (TicketsID) REFERENCES TicketsID(TicketsID),
+    -- Role ENUM('admin', 'customer') DEFAULT 'customer', maybe?!?!?
+);
+
+CREATE TABLE WalkIn (
+    WalkIn INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (TicketsID),
+    FOREIGN KEY (TicketsID) REFERENCES TicketsID(TicketsID), 
+)
